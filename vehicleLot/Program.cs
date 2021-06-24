95fcb7c3f1e25765ee73a30713966ef738a5ab4d
Giattino <40548@nelnet.net>
2021-06-24 18:12:16 -0400

    accessiblity and extends

---
+++ vehicleLot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace vehicleLot
+{
+    class Program
+    {
+        public class Vehicle
+        {
+            protected string VechileMake { get; set; }
+            protected string VechileModle { get; set; }
+            protected string VechileMilage { get; set; }
+
+        }
+
+
+        public class Semi : Vehicle
+        {
+            public string EighteenWheels { get; set; }
+            public int PassengerCount = 2;
+            public int CubicFeetSpace = 500;
+
+        }
+
+        public class Car : Vehicle
+        {
+            public int Wheels = 4;
+            public int PassengerCount = 5;
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+     
+}
